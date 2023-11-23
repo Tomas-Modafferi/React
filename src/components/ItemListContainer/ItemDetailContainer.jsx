b96906f33b540a7ef5693d93a925976fe1e736e7
tomas modafferi <tomasmodafferi@gmail.com>
2023-11-23 12:30:57 -0300

    detail

--- src/components/ItemListContainer/ItemDetailContainer.jsx
+++ src/components/ItemListContainer/ItemDetailContainer.jsx
@@ -9,6 +9,7 @@ import {
   where,
   documentId,
 } from "firebase/firestore";
+import CardItem from './CardItem';
 
 
 const ItemDetailContainer = () => {
@@ -16,8 +17,100 @@ const ItemDetailContainer = () => {
   const{item, loading} = useGetItem(id)
 
   return (
-    <div>ItemDetailContainer</div>
+    <div>
+      {item.map((data) =>{
+        return <CardItem item={data} key={data.id}/>
+      })}
+    </div>
   )
 }
 
 export default ItemDetailContainer
+// import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+
+
+// // FIREBASE
+// import { db } from "../../firebase/firebaseConfig";
+// import {
+//   collection,
+//   query,
+//   getDocs,
+//   where,
+//   documentId,
+// } from "firebase/firestore";
+
+// // COMPONENTS
+// import CardItem from "./CardItem";
+
+// const ItemDetailContainer = () => {
+//   const [itemsData, setItemsData] = useState([]);
+
+//   const { id } = useParams();
+
+//   useEffect(() => {
+//     const getItems = async () => {
+//       const q = query(
+//         collection(db, "items"),
+//         where(documentId(), "==", id)
+//       );
+//       const docs = [];
+//       const querySnapshot = await getDocs(q);
+//       querySnapshot.forEach((doc) => {
+//         docs.push({ ...doc.data(), id: doc.id }); 
+//       });
+//       setItemsData(docs);
+//     };
+//     getItems();
+//   }, [id]);
+
+//   return (
+//     <div className="ItemDetailContainer">
+//       {itemsData.map((items) => {
+//         return <CardItem items={items} key={items.id} />;
+//       })}
+//     </div>
+//   );
+// };
+
+// export default ItemDetailContainer;
+
+
+// import React , {useState, useEffect}from 'react'
+// import useGetItem from '../../hooks/useGetItem'
+// import { useParams } from 'react-router-dom'
+// import { db } from "../../firebase/firebaseConfig";
+// import {
+//   collection,
+//   query,
+//   getDocs,
+//   where,
+//   documentId,
+// } from "firebase/firestore";
+// import CardItem from './CardItem';
+
+
+// const ItemDetailContainer = () => {
+//   const [itemData, setItemData] = useState([])
+//   const {id} = useParams()
+
+//   useEffect(()=> {
+//     const getItems = async () => {
+      
+//       const q = query(collection(db, "items"), where(documentId(), "===", id))
+//       const docs = []
+//       const querySnapshot= await getDocs (q);
+//       querySnapshot.forEach((doc) => {
+//         docs.push({...doc.data(), id:doc.id});
+//       });
+//       setItemData(docs);
+//     };
+//     getItems();
+//   }, [])
+
+//   return (
+//     <div>ItemDetailContainer</div>
+//   )
+// }
+
+// export default ItemDetailContainer
--- src/components/body/MediaCard.jsx
+++ src/components/body/MediaCard.jsx
@@ -75,27 +75,3 @@ const MediaCard = ()=> {
   }
   export default MediaCard;
 
-//   <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-//   <CardContent sx={{ flex: '1 0 auto' }}>
-//     <Typography component="div" variant="h5">
-//       game-center
-//     </Typography>
-//     <Typography variant="subtitle1" color="text.secondary" component="div">
-//       Instagram
-//     </Typography>
-//     <Typography component="div" variant="h5">
-//       game.center
-//     </Typography>
-//     <Typography variant="subtitle1" color="text.secondary" component="div">
-//       Twitter
-//     </Typography>
-//     <Typography component="div" variant="h5">
-//       game_center
-//     </Typography>
-//     <Typography variant="subtitle1" color="text.secondary" component="div">
-//       Facebook
-//     </Typography>
-//   </CardContent>
-//   <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-//   </Box>
-// </Box>
